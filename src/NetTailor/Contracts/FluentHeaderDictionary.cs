9b49bb6369ce2c39340abd770ec71d8541ce9c90
Kirill Filippenko <filippenko.kir@gmail.com>
2023-03-21 10:37:48 +0400

    adding array pool renting while creating header set

--- src/NetTailor/Contracts/FluentHeaderDictionary.cs
+++ src/NetTailor/Contracts/FluentHeaderDictionary.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections;
+﻿using System.Buffers;
+using System.Collections;
 using System.Net.Http.Headers;
 
 namespace NetTailor.Contracts;
 
-public class FluentHeaderDictionary : IEnumerable<KeyValuePair<string, IEnumerable<string>>>
+public class FluentHeaderDictionary : IEnumerable<KeyValuePair<string, IEnumerable<string>>>, IDisposable
 {
-    private readonly Dictionary<string, IEnumerable<string?>> _dictionary;
+    private readonly List<KeyValuePair<string, IEnumerable<string>>> _headers;
     
     public FluentHeaderDictionary()
     {
-        _dictionary = new Dictionary<string, IEnumerable<string?>>();
+        var rented = ArrayPool<KeyValuePair<string, IEnumerable<string>>>.Shared.Rent(10);
+        _headers = new List<KeyValuePair<string, IEnumerable<string>>>(rented);
     }
     
     public AuthenticationHeaderValue? AuthenticationHeader { get; private set; }
@@ -20,24 +22,29 @@ public class FluentHeaderDictionary : IEnumerable<KeyValuePair<string, IEnumerab
         return this;
     }
 
-    public FluentHeaderDictionary Add(string name, string? value)
+    public FluentHeaderDictionary Add(string name, string value)
     {
         return Add(name, new[] { value });
     }
 
-    public FluentHeaderDictionary Add(string name, string?[] values)
+    public FluentHeaderDictionary Add(string name, string[] values)
     {
-        _dictionary.Add(name, values);
+        _headers.Add(new KeyValuePair<string, IEnumerable<string>>(name, values));
         return this;
     }
-
+    
     public IEnumerator<KeyValuePair<string, IEnumerable<string>>> GetEnumerator()
     {
-        return _dictionary.GetEnumerator()!;
+        return _headers.GetEnumerator();
     }
 
     IEnumerator IEnumerable.GetEnumerator()
     {
         return GetEnumerator();
+    }
+    
+    public void Dispose()
+    {
+        ArrayPool<KeyValuePair<string, IEnumerable<string>>>.Shared.Return(_headers.ToArray(), true);
     }
 }
